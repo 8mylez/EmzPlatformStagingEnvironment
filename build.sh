5091b84f3537d8d4fca18073a86242cc01fde394
Alexander Wolf <alexander.wolf@8mylez.com>
2021-06-24 11:08:02 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+commit=$1
+if [ -z ${commit} ]; then
+    commit=$(git tag | tail -n 1)
+    if [ -z ${commit} ]; then
+        commit="master";
+    fi
+fi
+
+# Build new release
+mkdir -p EmzPlatformStagingEnvironment
+git archive ${commit} | tar -x -C EmzPlatformStagingEnvironment
+zip -r EmzPlatformStagingEnvironment-${commit}.zip EmzPlatformStagingEnvironment
